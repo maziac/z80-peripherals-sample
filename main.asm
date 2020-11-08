b93c8f01687037aa38fe372d349f32e21f688a14
maziac <thomas.busse@gmx.net>
2020-11-08 18:52:37 +0100

    Interrupts.

--- main.asm
+++ main.asm
@@ -10,12 +10,54 @@ main:
     ; Setup
     di
     ld sp,stack_top
+    im 1
+    nop
+    nop
+    nop
 
-    ld bc,0x9000
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    ei
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+
+    ld bc,0x9001
 
 loop:
     in a,(c)
     jr loop
+
+
+    defs $0038-$
+im1_int:
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    ei
+    reti
+
+
+    defs $0066-$
+
+nmi_int:
+    ld c,9
+    ld e,8
+    retn
 
 
 
